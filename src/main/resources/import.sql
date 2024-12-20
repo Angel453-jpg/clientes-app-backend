0e1c5ededaa75c62346a2bbcebca354a5d98e56b
Angel <angelin09ozoz@gmail.com>
2024-12-19 18:25:30 -0600

    Agregando datos de prueba de mysql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO clientes (create_at, email, last_name, name) VALUES ('2024-12-19', 'angelin09ozoz@gmail.com', 'Meneses González', 'Ángel Gabriel');
+INSERT INTO clientes (create_at, email, last_name, name) VALUES ('2024-12-19', 'rafa@gmail.com', 'Meneses González', 'Rafael Adrian');
+INSERT INTO clientes (create_at, email, last_name, name) VALUES ('2024-12-19', 'felipa@gmail.com', 'González', 'Felipa');
