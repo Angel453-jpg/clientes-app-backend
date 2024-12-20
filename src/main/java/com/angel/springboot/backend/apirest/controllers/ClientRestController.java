45851735b64aabae5c993af5912b950e8101308c
Angel <angelin09ozoz@gmail.com>
2024-12-19 18:15:31 -0600

    Creando Controlador Client y se configura el método index.

---
+++ src/main/java/com/angel/springboot/backend/apirest/controllers/ClientRestController.java
@@ -0,0 +1,26 @@
+package com.angel.springboot.backend.apirest.controllers;
+
+import com.angel.springboot.backend.apirest.models.Client;
+import com.angel.springboot.backend.apirest.services.IClientService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ClientRestController {
+
+    private final IClientService clientService;
+
+    public ClientRestController(IClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/clients")
+    public List<Client> index() {
+        return clientService.findAll();
+    }
+
+}
