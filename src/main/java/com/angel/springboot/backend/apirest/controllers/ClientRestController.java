8fd612a9303ba26038f57a5ad5561b6464112a9d
Angel <angelin09ozoz@gmail.com>
2024-12-19 18:36:28 -0600

    Agregando CrossOrigin para la comunicación de nuestra Aplicación con angular.

--- src/main/java/com/angel/springboot/backend/apirest/controllers/ClientRestController.java
+++ src/main/java/com/angel/springboot/backend/apirest/controllers/ClientRestController.java
@@ -2,12 +2,14 @@ package com.angel.springboot.backend.apirest.controllers;
 
 import com.angel.springboot.backend.apirest.models.Client;
 import com.angel.springboot.backend.apirest.services.IClientService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClientRestController {
