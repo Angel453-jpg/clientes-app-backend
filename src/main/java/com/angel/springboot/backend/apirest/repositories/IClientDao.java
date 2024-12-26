dba1f44b814f9694a57e5168580dfd5ae7b5a778
Angel <angelin09ozoz@gmail.com>
2024-12-26 14:23:30 -0600

    Agregando paginación en el proyecto de Spring, se modifica el controlador, la implementación del servicio, el dao y el service.

--- src/main/java/com/angel/springboot/backend/apirest/repositories/IClientDao.java
+++ src/main/java/com/angel/springboot/backend/apirest/repositories/IClientDao.java
@@ -1,7 +1,7 @@
 package com.angel.springboot.backend.apirest.repositories;
 
 import com.angel.springboot.backend.apirest.models.Client;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IClientDao extends CrudRepository<Client,Long> {
+public interface IClientDao extends JpaRepository<Client,Long> {
 }
--- src/main/java/com/angel/springboot/backend/apirest/services/IClientService.java
+++ src/main/java/com/angel/springboot/backend/apirest/services/IClientService.java
@@ -1,12 +1,16 @@
 package com.angel.springboot.backend.apirest.services;
 
 import com.angel.springboot.backend.apirest.models.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IClientService {
 
     List<Client> findAll();
+
+    Page<Client> findAll(Pageable pageable);
 
     Client findById(Long id);
 
